9fa29ce3f4e2a3b7bb18c01cc6c010a7cd14745b
Rishabh <rishabhkumarnigam@gmail.com>
2025-06-28 20:47:09 +0530

    Module 3 :- Add audio generation functionality to AiModelController and AudioSpeechModelService

--- Spring AI MasterClass/Spring-AI-MasterClass/src/main/java/dev/rishabh/spring_ai/controller/AiModelController.java
+++ Spring AI MasterClass/Spring-AI-MasterClass/src/main/java/dev/rishabh/spring_ai/controller/AiModelController.java
@@ -1,5 +1,6 @@
 package dev.rishabh.spring_ai.controller;
 
+import dev.rishabh.spring_ai.service.AudioSpeechModelService;
 import dev.rishabh.spring_ai.service.ChatModelService;
 import dev.rishabh.spring_ai.service.ImageModelService;
 import lombok.RequiredArgsConstructor;
@@ -14,6 +15,7 @@ public class AiModelController {
 
     private final ChatModelService chatModelService;
     private final ImageModelService imageModelService;
+    private final AudioSpeechModelService audioSpeechModelService;
 
     @GetMapping("/generate-text-with-ai")
     public String generateAiText(@RequestParam String prompt) {
@@ -25,6 +27,23 @@ public class AiModelController {
                                              @RequestParam String model) {
         return ResponseEntity.ok(imageModelService.generateImage(prompt,model));
     }
+
+    @PostMapping(value = "/generate-audio-with-ai", produces = "audio/mpeg")
+    public ResponseEntity<byte[]> generateAudioWithAi(@RequestParam String text) {
+        byte[] audioData = audioSpeechModelService.tts(text);
+        return ResponseEntity.ok()
+                .contentType(MediaType.valueOf("audio/mpeg"))
+                .body(audioData);
+    }
+
+
+
+
+
+
+
+
+
 
 
 }
--- Spring AI MasterClass/Spring-AI-MasterClass/src/main/java/dev/rishabh/spring_ai/service/AudioSpeechModelService.java
+++ Spring AI MasterClass/Spring-AI-MasterClass/src/main/java/dev/rishabh/spring_ai/service/AudioSpeechModelService.java
@@ -1,4 +1,28 @@
 package dev.rishabh.spring_ai.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.ai.openai.OpenAiAudioSpeechModel;
+import org.springframework.ai.openai.OpenAiAudioSpeechOptions;
+import org.springframework.ai.openai.api.OpenAiAudioApi;
+import org.springframework.ai.openai.audio.speech.SpeechPrompt;
+import org.springframework.ai.openai.audio.speech.SpeechResponse;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class AudioSpeechModelService {
+
+    private final OpenAiAudioSpeechModel openAiAudioSpeechModel;
+
+    public byte[] tts(String text) {
+        SpeechPrompt speechPrompt = new SpeechPrompt(text,
+                OpenAiAudioSpeechOptions.builder()
+                        .model(OpenAiAudioApi.TtsModel.TTS_1.value)
+                        .voice(OpenAiAudioApi.SpeechRequest.Voice.ALLOY.value)
+                        .speed(1.0f)
+                        .responseFormat(OpenAiAudioApi.SpeechRequest.AudioResponseFormat.MP3)
+                        .build());
+        SpeechResponse speechResponse = openAiAudioSpeechModel.call(speechPrompt);
+        return speechResponse.getResult().getOutput();
+    }
 }
